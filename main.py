efbec59eaa42cf948a1a1e5cc0869ca922db261d
Gabriel Marczak <denetgabriel@gmail.com>
2023-01-26 22:56:01 +0100

    Fix small things

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from faker import Faker
-
-fake = Faker()
-lista = []
+import time
 
 class BaseContact:
     def __init__(self, name, family, phone, email):
@@ -9,27 +7,28 @@ class BaseContact:
         self.family = family
         self.phone = phone
         self.email = email
-        self.contact_phone = self.phone
+        self.contact_phone = self.phone 
 
     def __str__(self):
         return f"{self.name} {self.family} - {self.email}"
 
-    def contact(self):
+    def contact(self): # w 10 linii kodu ustawiam zmienną contact_phone na prywatny numer, w klasie BusinessContact jest na służbowym numerze
         print(f"Wybieram numer {self.contact_phone} i dzwonię do {self.name} {self.family}")
 
     @property
     def label_length(self):
-        return f"{len(self.name)} {len(self.family)}"
+        return len(self.name) + len(self.family) + 1
 
 class BusinessContact(BaseContact):
-    def __init__(self, role, company, wphone, *args, **kwargs):
+    def __init__(self, role, company, workphone, *args, **kwargs):
        super().__init__(*args, **kwargs)
        self.role = role
        self.company = company
-       self.wphone = wphone
-       self.contact_phone = self.wphone
+       self.workphone = workphone
+       self.contact_phone = self.workphone
     
 def create_contact(typ, ilosc):
+    lista = []
     for _ in range(ilosc):
         if typ == "basic":
             n = fake.first_name()
@@ -46,8 +45,13 @@ def create_contact(typ, ilosc):
             c = fake.company()
             wp = fake.phone_number()
             lista.append(BusinessContact(r, c, wp, n, f, p, e))
+    return lista        
 
-create_contact("business", 3)
-lista[0].contact()
-print(lista[0].email)
-print(lista[0].label_length)
+if __name__ == "__main__":
+    fake = Faker()
+    starttime = time.time()
+    test = create_contact("business", 5)
+    endtime = time.time()
+    print(endtime - starttime)
+    print(test[1])
+    print(test[1].label_length)
